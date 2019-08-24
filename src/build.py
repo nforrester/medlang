d30263c99025495b04d7431ab43d76c8c27619f2
Neil Forrester <neil@nforrester.net>
2019-08-24 09:07:21 -0700

    Refactor configuration to make reuseable templates

--- src/build.py
+++ src/build.py
@@ -1,10 +1,44 @@
 #!/usr/bin/env python3
 
+import jinja2
+import json
 import os
 import shutil
 import sys
 
-import process_page
+
+def load_config(path):
+    with open(path) as f:
+        return json.load(f)
+
+
+def write_output(path, content):
+    output_dir = os.path.dirname(path)
+    if not os.path.isdir(output_dir):
+        os.makedirs(output_dir)
+    assert os.path.isdir(output_dir)
+
+    with open(path, 'w') as f:
+        f.write(content)
+    assert os.path.isfile(path)
+
+
+def environment():
+    loader = jinja2.FileSystemLoader('.')
+    env = jinja2.Environment(loader=loader, autoescape=jinja2.select_autoescape())
+    env.undefined = jinja2.StrictUndefined
+    return env
+
+
+def process_page(env, config, output_dir, filename):
+    page = config['pages'][filename]
+
+    template = env.get_template(os.path.join('data/templates', page['template']))
+    output = template.render(
+        filename=filename,
+        page=page,
+        **config)
+    write_output(os.path.join(output_dir, filename), output)
 
 
 def main():
@@ -12,18 +46,15 @@ def main():
     if os.path.isdir(output_dir):
         shutil.rmtree(output_dir)
 
-    env = process_page.environment()
-
-    def page(name):
-        process_page.process_page(
-            env,
-            os.path.join('data/templates', name + '.html'),
-            os.path.join('data/config', name + '.json'),
-            os.path.join(output_dir, name + '.html'))
-
     shutil.copytree('data/plain', output_dir)
 
-    page('index')
+    config = load_config('data/config/config.json')
+    env = environment()
+
+    for filename in config['pages'].keys():
+        process_page(env, config, output_dir, filename)
+
+    return True
 
 
 if __name__ == '__main__':
